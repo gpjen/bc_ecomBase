5831f9f2032023e03d1881d174b1df218583e5d6
Gandi Purna Jen <nyonggpj9995@gmail.com>
2022-03-19 21:34:20 +0700

    update

--- src/midleware/auth.js
+++ src/midleware/auth.js
@@ -1,10 +1,16 @@
+//-- IMPORT PACKEGE
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
+//-- EXPORTS AUTH
 exports.auth = async (req, res, next) => {
+
+    //-- GET HEADER AUTHORIZATION
 
     const authHeader = req.header('Authorization')
     const token = authHeader && authHeader.split(" ")[1]
+
+    //-- CHECKING TOKEN
 
     if (!token) return res.status(200).json({
         status: 'failed',
@@ -12,11 +18,13 @@ exports.auth = async (req, res, next) => {
     })
 
     try {
+        //-- VERTIFY TOKEN
         const verified = jwt.verify(token, process.env.DT_SECRET_KEY)
         req.user = verified
         next()
 
     } catch (error) {
+        //-- ERROR HANDLING INVALID TOKEN
         res.status(500).json({
             status: 'failed',
             message: 'invalid token'
